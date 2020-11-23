fb206f1ba7508be1702e6b9e1386151a8096ea1f
Adrian Cejudo Garcia <adrian.cejudogarcia@altran.com>
2020-11-23 19:18:01 +0100

    angular-form. Implementation of FormGroup on app.component

--- angular-form/src/app/app.component.ts
+++ angular-form/src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-form';
+  public formPerson: FormGroup;
+  public submitted: boolean;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.submitted = false;
+    this.formPerson = this.formBuilder.group({
+      name: ['', [Validators.required]]
+    })
+  }
+
+  ngOnInit(): void {
+
+  }
+
+  get f() {
+    return this.formPerson.controls;
+  }
+
+  onSubmit() {
+    this.submitted = true;
+  }
+  onClear() {
+    this.submitted = false;
+    this.formPerson.reset();
+  }
+
 }
