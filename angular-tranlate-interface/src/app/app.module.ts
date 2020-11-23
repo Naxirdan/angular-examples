6c312b31257036ec6da8a609ca3ff1c999ded54c
Adrian Cejudo Garcia <adrian.cejudogarcia@altran.com>
2020-11-23 19:32:20 +0100

    angular-translate-interface. Update configuration app.module.ts

--- angular-tranlate-interface/src/app/app.module.ts
+++ angular-tranlate-interface/src/app/app.module.ts
@@ -1,8 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClient } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -16,3 +20,7 @@ import { AppComponent } from './app.component';
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
