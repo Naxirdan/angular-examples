912b97cdd38e8fc07b27dca3bdb845dde4f11655
Adrian Cejudo Garcia <adrian.cejudogarcia@altran.com>
2020-11-25 21:29:12 +0100

    angular-ngxr. Add configuration on app.module.ts and initial config con core/store

--- angular-ngrx/src/app/app.module.ts
+++ angular-ngrx/src/app/app.module.ts
@@ -3,6 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StoreModule } from '@ngrx/store';
+import { environment } from 'src/environments/environment';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { EffectsModule } from '@ngrx/effects';
+import { reducers } from 'src/app/core/store';
 
 @NgModule({
   declarations: [
@@ -10,7 +15,20 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StoreModule.forRoot(reducers,
+      {
+        metaReducers: !environment.production ? [] : [],
+        runtimeChecks: {
+          strictActionImmutability: true,
+          strictStateImmutability: true
+        }
+      }
+    ),
+    !environment.production
+      ? StoreDevtoolsModule.instrument()
+      : [],
+    EffectsModule.forRoot([]),
   ],
   providers: [],
   bootstrap: [AppComponent]
